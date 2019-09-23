26c49d4584a36840701694c28207bb2797b23b7d
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2019-09-23 14:16:55 +0200

    Update style.js

--- style.js
+++ style.js
@@ -22,7 +22,7 @@
 				{ name: 'Título 4', element: 'h4', attributes: { 'class': 'bck-title4'} },
 				{ name: 'Título 5', element: 'h4', attributes: { 'class': 'bck-title5'} },
 				{ name: 'Título 6', element: 'h4', attributes: { 'class': 'bck-title6'} },
-				{ name: 'Título trabajo cotidiano', element: 'h4', attributes: { 'class': 'bck-title7'} },
+				{ name: 'Título Intro', element: 'h4', attributes: { 'class': 'bck-title7'} },
 				{ name: 'Título Analice', element: 'h4', attributes: { 'class': 'bck-title8'} },
 
 				{ name: 'Énfasis', element: 'span', attributes: { 'class': 'bck-enfasis' }},
