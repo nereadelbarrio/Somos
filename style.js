fdb5cb3ea54c8b614cbb7bfc4041f67292deff97
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2019-10-08 17:13:42 +0200

    Update style.js

--- style.js
+++ style.js
@@ -85,271 +85,176 @@
 		},
 
 		init: function () {
-			var that = this;
-			this.parent.init.call(this);
-			this.addActivityTitle();
-			this.addPageNumber();
-			this.getActualUnitActivities();
-			blink.events.on("course_loaded", function(){
-           			that.enableSliders();
-      	  		});
-			this.formatCarouselindicators();
-			this.addSlideNavigators();
+			this.parent.init.call(this.parent, this);
 			this.parent.initInfoPopover();
+			this.getActualUnitActivities();
+			blink.activity.navigateBetweenActivities();
 		},
 
 		removeFinalSlide: function () {
-			this.parent.removeFinalSlide.call(this, true);
+			this.parent.removeFinalSlide.call(this.parent, this, true);
 		},
 
-		addActivityTitle: function () {
-			if (!blink.courseInfo || !blink.courseInfo.unit) return;
-			$('.libro-left').find('.title').html(function () {
-				return $(this).html() + ' > ' + blink.courseInfo.unit;
-			})
+		/**
+		 * @summary Gets the activity type subunits of the actual unit.
+		 * @return {Object} Object of the actual unit filtering the not activity type subunits
+		 */
+		getActualUnitActivities: function () {
+			var curso = blink.getCourse(idcurso),
+				that = this,
+				units,
+				unitSubunits,
+				actualActivity,
+				unitActivities = [];
+
+			curso.done(function () {
+				units = curso.responseJSON.units;
+
+				$.each(units, function () {
+					if (this.title && this.title == blink.courseInfo.unit) {
+						unitSubunits = this.subunits.concat(this.resources);
+					}
+				});
+
+				actualActivity = _.find(unitSubunits, function (subunit) {
+					return subunit.id == idclase;
+				});
+
+				if (actualActivity.level == '6') {
+					unitActivities.push(actualActivity);
+				} else {
+					unitActivities = _.filter(unitSubunits, function (subunit) {
+						return subunit.type == 'actividad' && subunit.level !== '6';
+					});
+				}
+
+				that.subunits = unitActivities;
+			}).done(function () {
+				blink.events.trigger('course_loaded');
+			});
+		},
+
+		/**
+		 * @summary Getting active slide position in relation with the total of the
+		 *          unit slides.
+		 * @param {Array} $subunits Array of activity type objects
+		 * @return {int} Slide position
+		 */
+		getActualSlideNumber: function (subunits) {
+			var actualSlideIndex = $('.swipeview-active').attr('data-page-index'),
+				actualSlide = 1;
+
+			for (var i in subunits) {
+				if (subunits[i].id && parseInt(subunits[i].id) != idclase) {
+					actualSlide += parseInt(subunits[i].pags);
+				} else {
+					actualSlide += parseInt(actualSlideIndex);
+					break;
+				}
+			}
+
+			return actualSlide;
+		},
+
+		addSlideNavigators: function () {
+			var $navigator = $('<div class="navigator"><div class="main-navigator"><div class="left"></div><div class="right"></div></div></div>'),
+				$leftControl = $('.left.slider-control').clone(),
+				$rightControl = $('.right.slider-control').clone()
+
+			var esdeber = blink.activity.esdeber;
+			var idClaseOriginal = blink.activity.idClaseOriginal;
+
+			$leftControl.add($rightControl).find('span').remove();
+
+			var slideIndex = 0; // se utiliza como indice para saltarnos los concatenados en el each
+			var slidesNav = $('.item-container');
+			// Filtramos para que solo coja las slides que no son final slide para iterar sobre ellas
+			slidesNav = slidesNav.filter(function (element) {
+				if ($(slidesNav[element]).find('#final').length > 0) {
+					return false;
+				}
+				return true;
+			});
+
+			slidesNav.each(function (index, element) {
+				var $itemNavigator = $navigator.clone(),
+					$left, $right, hasLeft = false;
+
+				var prevSlide,
+					prevIndex = slideIndex - 1;
+				// si  hay una slide anterior se recorre hacia atras hasta que no haya concatenados
+				// si estoy en la slide 0 no se pinta
+				while (prevIndex >= 0) {
+					if (prevSlide = window['t' + prevIndex + '_slide']) {
+						if (prevSlide.isConcatenate && (((esdeber) ? idClaseOriginal != 0 : true))) {
+							prevIndex--;
+						} else {
+							$left = $leftControl.clone();
+							$left.append('<span class="title">' + stripHTML(prevSlide.title) + '</span>');
+							$itemNavigator.find('.left').append($left);
+							hasLeft = true;
+							break;
+						}
+					} else {
+						break;
+					}
+				}
+
+				slideIndex++;
+				var nextSlide;
+				// si  hay una slide siguiente se recorre hacia adelante hasta que no haya concatenados
+				// si estoy en la slide ultima no se pinta boton next
+				while (slideIndex < window.secuencia.length) {
+					if (nextSlide = window['t' + slideIndex + '_slide']) {
+						if (nextSlide.isConcatenate && (((esdeber) ? idClaseOriginal != 0 : true))) {
+							slideIndex++;
+						} else {
+							$right = $rightControl.clone();
+							$right.prepend('<span class="title">' + stripHTML(nextSlide.title) + '</span>');
+							$itemNavigator.find('.right').append($right);
+							hasLeft && $right.parent('.right').addClass('separator');
+							break;
+						}
+					} else {
+						break;
+					}
+				}
+				$(element).append($itemNavigator);
+			});
+			//Quitamos el on click listener ya que lo coge de blink.activity.navigateBetweenActivities()
+		},
+
+		handleScrollEnd: function () {
+			$('#top-navigator')
+				.removeClass('show_left')
+				.removeClass('show_right');
+
+			if (this.x < 0) {
+				$('#top-navigator').addClass('show_left');
+			}
+			if (this.x > this.maxScrollX) {
+				$('#top-navigator').addClass('show_right');
+			}
+
 		},
 
 		configEditor: function (editor) {
 			editor.dtd.$removeEmpty['span'] = false;
 		},
 
-		addPageNumber: function() {
-			$('.js-slider-item').each(function(i,e) {
-				var idPage = $(e).attr('id');
-				var page = parseInt(idPage.replace("slider-item-", ""))+1;
-				$(e).find('.header').prepend('<div class="single-pagination"><div class="page">'+page+'</div></div>');
-			});
-		},
-
-
-		formatCarouselindicators: function () {
-			var $navbarBottom = $('.navbar-bottom'),
-				$carouselIndicators = $('.slider-indicators').find('li');
-			$navbarBottom.find('li').tooltip('destroy');
-
-			var dropDown = '' +
-					'<div class="dropdown">' +
-						'<button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" role="button" aria-expanded="false">' +
-							'Índice' +
-							'<span class="caret"></span>' +
-						'</button>' +
-						'<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">';
-
-			var navigatorIndex = 0;
-			for (var index = 0; index < window.secuencia.length; index++) {
-				var slide = eval('t'+index+'_slide'),
-					slideTitle = slide.title;
-
-				if (slide.isConcatenate) continue;
-
-				dropDown += '<li role="presentation"><a role="menuitem">' + (navigatorIndex+1) + '. ' + stripHTML(slideTitle) + '</a></li>';
-				$navbarBottom.find('li').eq(navigatorIndex).html('<span title="'+ stripHTML(slideTitle) +'">'+(navigatorIndex+1)+'</span>');
-				navigatorIndex++;
-
-			};
-
-			dropDown += '' +
-						'</ul>' +
-					'</div>';
-
-			$navbarBottom
-				.attr('class', 'publisher-navbar')
-				.wrapInner('<div class="navbar-content"></div>')
-				.find('ol')
-					.before(dropDown)
-					.wrap('<div id="top-navigator"/>')
-					.end()
-				.find('.dropdown').find('li')
-					.on('click', function (event) {
-						$navbarBottom.find('ol').find('li').eq($(this).index()).trigger('click');
-					});
-
-			if (!blink.hasTouch) {
-				$navbarBottom
-					.find('ol').find('span')
-						.tooltip({
-							placement: 'bottom',
-							container: 'body'
-						});
-			}
-		},
-
-		/**
-         * @summary Gets the activity type subunits of the actual unit.
-         * @return {Object} Object of the actual unit filtering the not activity type subunits
-         */
-        getActualUnitActivities: function () {
-            var curso = blink.getCourse(idcurso),
-                that = this,
-                units,
-                unitSubunits,
-                unitActivities;
-
-            curso.done(function () {
-                units = curso.responseJSON.units;
-                $.each(units, function () {
-                    if (this.id && this.id == blink.courseInfo.IDUnit) {
-                        unitSubunits = this.subunits.concat(this.resources);
-                    }
-                });
-                unitActivities = _.filter(unitSubunits, function(subunit) {
-                    //BK-16866 Quitamos las actividades ocultas y las solo visibles para profesor para que no se pueda navegar a ellas.
-                    return subunit.type == 'actividad' && (!subunit.ocultar && !subunit.onlyVisibleTeachers);
-                });
-                that.subunits = unitActivities;
-	    	for (var i in that.subunits) {
-	    		if (that.subunits[i].id && that.subunits[i].id == idclase) {
-                        	$(".publisher-navbar").append('<span class="slide-counter" data-subunit-index="' + i +
-                            		'" data-subunit-pags="' + parseInt(that.subunits[i].pags) + '" style="visibility: hidden;">');
-			}
-                }
-            }).done(function(){
-                blink.events.trigger('course_loaded');
-            });
-        },
-	/**
-         * @summary Getting active slide position in relation with the total of the
-         * unit slides.
-         * @param {Array} $subunits Array of activity type objects
-		  * @return {int} Slide position
-         */
-        getActualSlideNumber: function (subunits) {
-            var actualSlideIndex = $('.swipeview-active').attr('data-page-index'),
-                actualSlide = 1;
-
-            for (var i in subunits) {
-                if (subunits[i].id && parseInt(subunits[i].id) != idclase) {
-                    actualSlide += parseInt(subunits[i].pags);
-                } else {
-                    actualSlide += parseInt(actualSlideIndex);
-                    break;
-                }
-            }
-
-            return actualSlide;
-        },
-		
-	
-		
-		  /**
-         * @summary Enables all slider controls and disables when appropiate
-         */
-        enableSliders: function () {
-            //BK-15715 Traemos subunit_index para saber si la actividad esta oculta. Si subunit_index es NaN, la actividad es oculta y no tiene que navegar entre actividades, solo entre las slides de la actividad
-            var subunit_index = parseInt($('.slide-counter').attr('data-subunit-index'));
-            // Removes disabled class to all navigation buttons and applies
-            // just if its first or last slide of all activities
-            $('.slider-control, .slider-navigator').removeClass('disabled');
-
-            // Navigation change depending on whether the slides are accessed from
-            // a book or from a homework link or similar
-            // BK-15715 Comprobamos a mayores que subunit_index no sea NaN para habilitar la navegacion entre actividades.
-            if (this.subunits.length !== 0 && !isNaN(subunit_index)) {
-                if (this.getActualSlideNumber(this.subunits) == 1) {
-                    $('.slider-control.left, .slider-navigator.left').addClass('disabled');
-                }
-                if (this.getActualSlideNumber(this.subunits) == this.totalSlides) {
-                    $('.slider-control.right, .slider-navigator.right').addClass('disabled');
-                }
-            } else {
-                if (window.activeSlide == 0) {
-                    $('.slider-control.left, .slider-navigator.left').addClass('disabled');
-                }
-                if (window.activeSlide + 1 == window.secuencia.length) {
-                    $('.slider-control.right, .slider-navigator.right').addClass('disabled');
-                }
-            }
-        },
-		
-	addSlideNavigators: function () {
-            var idgrupo = window.idgrupo,
-                idalumno = window.idalumno,
-                slideNavParams = '';
-
-            if (idgrupo) slideNavParams += '&idgrupo=' + idgrupo;
-            if (idalumno) slideNavParams += '&idalumno=' + idalumno;
-
-            blink.events.on("course_loaded", function(){
-                var that = blink.activity.currentStyle,
-                    subunit_index = parseInt($('.slide-counter').attr('data-subunit-index'));
-
-                $('.slider-control').off('click');
-
-                // Navigation change depending on whether the slides are accessed from
-                // a book or from a homework link or similar
-                if (that.subunits.length !== 0) {
-                    // Slider controls must allow navigation among all the activity subunits
-                    // in the current unit.
-                    $('.left.slider-control, .left.slider-navigator').click(function () {
-                        if (!$(this).hasClass('disabled')) {
-                            if(activeSlide == 0) {
-                                redireccionar('/coursePlayer/clases2.php?editar=0&idcurso=' +
-                                    idcurso + '&idclase=' + that.subunits[subunit_index - 1].id + '&modo=0&numSec=' +
-                                    that.subunits[subunit_index - 1].pags + slideNavParams, false, undefined);
-                            } else {
-                                blink.activity.showPrevSection();
-                        }
-                        }
-                    });
-                    $('.right.slider-control, .right.slider-navigator').click(function () {
-                        if (!$(this).hasClass('disabled')) {
-                            //BK-15715 Añadimos la condicion que subunit_index no sea NaN para que se active la navegacion entre slides
-                            if(!isNaN(subunit_index) && activeSlide == parseInt(that.subunits[subunit_index].pags) - 1) {
-                                if (!that.subunits[subunit_index + 1].ocultar)
-                                redireccionar('/coursePlayer/clases2.php?editar=0&idcurso=' +
-                                    idcurso + '&idclase=' + that.subunits[subunit_index + 1].id + '&modo=0&numSec=1'+slideNavParams,
-                                    false, undefined);
-                            } else {
-                                blink.activity.showNextSection();
-                            }
-                        }
-                    });
-                } else {
-                    $('.left.slider-control, .left.slider-navigator').click(function () {
-                        blink.activity.showPrevSection();
-                    });
-                    $('.right.slider-control, .right.slider-navigator').click(function () {
-                        blink.activity.showNextSection();
-                    });
-                }
-
-                $(document).ready(function() {
-                    blink.events.on('showSlide:after', function() {
-                        that.enableSliders();
-                    });
-		});
-            });
-        },
-		
 	};
 
-	SomosStyle.prototype = _.extend({}, new blink.theme.styles.basic(), SomosStyle.prototype);
+	SomosStyle.prototype = _.extend({}, new blink.theme.styles.fancy(), SomosStyle.prototype);
 
 	blink.theme.styles.Somos = SomosStyle;
 
-})( blink );
+})(blink);
 
 $(document).ready(function () {
 
-    $('.item').find('.header').find('.title')
-		.filter(function () {
-			return $(this).find('.empty').length;
-		}).hideBlink();
-
-    $('.item').find('.header').find('.title')
-		.filter(function () {
-			return !$(this).find('.empty').length;
-		})
-		.each(function () {
-			var $header = $(this).find('h3');
-			$header.length && $header.html($header.html().replace(' ', ''));
-		});
-
-	// BK-8433 cambiamos el logo de las slides por el del dominio
 	var src_logo = $('.content_type_clase_Somos').find('.logo_slide').attr('logo_dominio');
-	if (typeof(src_logo) != 'undefined' && src_logo && src_logo != '' && src_logo.indexOf('gif1x1.gif') == -1) {
-		$('.content_type_clase_Somos').find('.logo-publisher').css('background-image', "url('"+src_logo+"')");
+	if (typeof (src_logo) != 'undefined' && src_logo && src_logo != '' && src_logo.indexOf('gif1x1.gif') == -1) {
+		$('.content_type_clase_Somos').find('.logo-publisher').css('background-image', "url('" + src_logo + "')");
 	}
 
 });
-
